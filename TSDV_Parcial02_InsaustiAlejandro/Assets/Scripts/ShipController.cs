27f9ecc0aaa0a0a63d762ff5940eea5c9b0eac3b
Ale <alejandroninsausti@gmail.com>
2021-06-28 00:23:16 -0300

    Add Ship Controller

---
+++ TSDV_Parcial02_InsaustiAlejandro/Assets/Scripts/ShipController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    #region Variables
+    [SerializeField] float rocketPower;
+    [SerializeField] float rotationSpeed;
+    [SerializeField] float gravity;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] GameObject rocketPropulsionEffect;
+    Vector2 rocketForce;
+    Vector3 rotation;
+    #endregion
+
+    #region Unity Methods
+    private void Start()
+    {
+        rocketForce = new Vector2(0,0);
+        rotation = new Vector3(0,0,0);
+        rocketPropulsionEffect.active = false;
+    }
+    private void Update()
+    {
+        UpdateGravity();
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))
+        {
+            AddRocketForce();   
+        }
+        else
+        {
+            SetRocketPropulsion(false);
+        }
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            Rotate();
+        }
+    }
+    #endregion
+
+    #region Methods
+    void UpdateGravity()
+    {
+        rb.gravityScale = gravity;
+    }
+    void SetRocketPropulsion(bool turnOn)
+    {
+        rocketPropulsionEffect.active = turnOn;
+    }
+    void AddRocketForce()
+    {
+        rocketForce.y = rocketPower;
+        rb.AddRelativeForce(rocketForce);
+        SetRocketPropulsion(true);
+    }
+    void Rotate()
+    {
+        //positive rotation uses right arrow and rotates to the left
+        //so rotation needs to be inverted to turn in the desired direction
+        rotation.z = -Input.GetAxis("Horizontal");
+        transform.Rotate(rotation);
+    }
+    #endregion
+}
---
+++ TSDV_Parcial02_InsaustiAlejandro/Assets/Scripts/ShipData.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ShipData : MonoBehaviour
+{
+     #region Variables
+	
+     #endregion
+
+     #region Unity Methods
+	
+     #endregion
+
+     #region Methods
+	
+     #endregion
+}
