55e2366283305711a627c9e82111cb1adca8b741
Ale <alejandroninsausti@gmail.com>
2021-06-28 01:56:05 -0300

    Add Terrain SOs

---
+++ TSDV_Parcial02_InsaustiAlejandro/Assets/Scripts/TerrainChunkScriptableObject.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Terrain Chunk Coordinates")]
+public class TerrainChunkScriptableObject : ScriptableObject
+{
+    [SerializeField] Vector2 leftEnd;
+    [SerializeField] Vector2 rightEnd;
+}
