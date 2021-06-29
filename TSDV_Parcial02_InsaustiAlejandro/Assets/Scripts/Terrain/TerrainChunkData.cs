63c69ca30e2112f62f4d6a6f343c93854e5b9149
alejandroninsausti <alejandroninsausti@gmail.com>
2021-06-29 13:20:10 -0300

    Add Terrain Chunk manager

--- TSDV_Parcial02_InsaustiAlejandro/Assets/Scripts/Terrain/TerrainChunkData.cs
+++ TSDV_Parcial02_InsaustiAlejandro/Assets/Scripts/Terrain/TerrainChunkData.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 public class TerrainChunkData : MonoBehaviour
 {
     #region Variables
+    public int chunkIndex;
     public bool onLeftSide;
-    public TerrainChunkScriptableObject SO;
-    [SerializeField] int chunkIndex;
+    public TerrainChunkScriptableObject SO { get; private set; }
     #endregion
 
     #region Unity Events
---
+++ TSDV_Parcial02_InsaustiAlejandro/Assets/Scripts/Terrain/TerrainChunkManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+struct Chunk
+{
+    public Transform transform;
+    public TerrainChunkData data;
+}
+public class TerrainChunkManager : MonoBehaviour
+{
+    #region Variables
+    [SerializeField] int chunksToGenerate;
+    [SerializeField] int typesOfChunk;
+    [SerializeField] Chunk[] chunks = new Chunk[2];
+    [SerializeField] GameObject chunkPrefab;
+    #endregion
+
+    #region Unity Events
+    private void Start()
+    {
+        for (int i = 0; i < chunks.Length; i++)
+        {
+            chunks[i] = GenerateChunk();
+        }
+    }
+    #endregion
+
+    #region Methods
+    Chunk GenerateChunk()
+    {
+        //generate chunk Game Object
+        GameObject go = Instantiate(chunkPrefab);
+        go.GetComponent<TerrainChunkData>().chunkIndex = Random.Range(1, typesOfChunk + 1);
+
+        //Get chunk data
+        Chunk chunk = new Chunk();
+        chunk.transform = go.transform;
+        chunk.data = go.GetComponent<TerrainChunkData>();
+        return chunk;
+    }
+    #endregion
+}
