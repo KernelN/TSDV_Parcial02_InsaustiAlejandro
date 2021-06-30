aa0b57a859a9ac54ffa378e7f256549a575b0999
alejandroninsausti <alejandroninsausti@gmail.com>
2021-06-29 22:05:42 -0300

    Add Background

--- TSDV_Parcial02_InsaustiAlejandro/Assets/Scripts/Ship/CameraController.cs
+++ TSDV_Parcial02_InsaustiAlejandro/Assets/Scripts/Ship/CameraController.cs
@@ -3,54 +3,87 @@
 public class CameraController : MonoBehaviour
 {
     #region Variables
-    [SerializeField] float followRadius;
+    public float followRadius;
+    [SerializeField] GameObject zoomedOutCamera;
+    [SerializeField] GameObject zoomedInCamera;
     [SerializeField] Transform player;
+    Transform currentCamera;
+    bool zoomIsOn = false;
+    LayerMask mapLayer;
     #endregion
 
     #region Unity Events
+    private void Start()
+    {
+        mapLayer = LayerMask.GetMask("Map");
+        currentCamera = zoomedOutCamera.transform;
+        player.GetComponent<ShipController>().OnAltitudeChange += AdjustZoom;
+    }
     private void Update()
     {
         FollowPlayer();
+    }
+    private void OnDisable()
+    {
+        player.GetComponent<ShipController>().OnAltitudeChange -= AdjustZoom;
     }
     #endregion
 
     #region Methods
     void FollowPlayer()
     {
-        if (Mathf.Abs(player.position.x - transform.position.x) > followRadius)
+        if (zoomIsOn && Mathf.Abs(player.position.x - currentCamera.position.x) > followRadius)
         {
-            transform.position = new Vector3(GetNewXAxis(), transform.position.y, transform.position.z);
+            currentCamera.position = new Vector3(GetNewXAxis(), currentCamera.position.y, currentCamera.position.z);
         }
-        if (Mathf.Abs(player.position.y - transform.position.y) > followRadius)
+        if (Mathf.Abs(player.position.y - currentCamera.position.y) > followRadius)
         {
-            transform.position = new Vector3(transform.position.x, GetNewYAxis(), transform.position.z);
+            currentCamera.position = new Vector3(currentCamera.position.x, GetNewYAxis(), currentCamera.position.z);
         }
     }
     float GetNewXAxis()
     {
-        if (player.position.x > transform.position.x + followRadius)
+        if (player.position.x > currentCamera.position.x + followRadius)
         {
             return player.position.x - followRadius;
         }
-        else if (player.position.x < transform.position.x - followRadius)
+        else if (player.position.x < currentCamera.position.x - followRadius)
         {
             return player.position.x + followRadius;
         }
 
-        return transform.position.x;
+        return currentCamera.position.x;
     }
     float GetNewYAxis()
     {
-        if (player.position.y > transform.position.y + followRadius)
+        if (player.position.y > currentCamera.position.y + followRadius)
         {
             return player.position.y - followRadius;
         }
-        else if (player.position.y < transform.position.y - followRadius)
+        else if (player.position.y < currentCamera.position.y - followRadius)
         {
             return player.position.y + followRadius;
         }
 
-        return transform.position.y;
+        return currentCamera.position.y;
+    }
+    void AdjustZoom(float playerDistanceToRock)
+    {
+        RaycastHit2D hit = Physics2D.CircleCast(player.localPosition, followRadius * 2, -player.up, 0, mapLayer);
+        if (zoomIsOn && hit.collider == null)
+        {
+            zoomIsOn = false;
+            zoomedInCamera.SetActive(false);
+            zoomedOutCamera.SetActive(true);
+            currentCamera = zoomedOutCamera.transform;
+        }
+        else if (!zoomIsOn && hit.collider != null)
+        {
+            zoomIsOn = true;
+            zoomedInCamera.gameObject.SetActive(true);
+            zoomedOutCamera.gameObject.SetActive(false);
+            currentCamera = zoomedInCamera.transform;
+        }
     }
     #endregion
 }
--- TSDV_Parcial02_InsaustiAlejandro/Assets/Scripts/Ship/ShipController.cs
+++ TSDV_Parcial02_InsaustiAlejandro/Assets/Scripts/Ship/ShipController.cs
@@ -1,20 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 public class ShipController : MonoBehaviour
 {
     #region Variables
+    public UnityAction<float> OnAltitudeChange;
+    [SerializeField] float shipTooCloseToRockDistance;
     [SerializeField] float rocketPower;
     [SerializeField] float rotationSpeed;
     [SerializeField] float gravity;
     [SerializeField] Rigidbody2D rb;
     [SerializeField] GameObject rocketPropulsionEffect;
+    LayerMask mapLayer;
     Vector2 rocketForce;
     Vector3 rotation;
+    float altitude;
     #endregion
 
     #region Unity Methods
     private void Start()
     {
+        mapLayer = LayerMask.GetMask("Map");
         rocketForce = new Vector2(0,0);
         rotation = new Vector3(0,0,0);
         rocketPropulsionEffect.SetActive(false);
@@ -22,6 +28,8 @@ public class ShipController : MonoBehaviour
     private void Update()
     {
         UpdateGravity();
+        CheckAltitude();
+
         if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))
         {
             AddRocketForce();   
@@ -30,6 +38,7 @@ public class ShipController : MonoBehaviour
         {
             SetRocketPropulsion(false);
         }
+
         if (Input.GetAxis("Horizontal") != 0)
         {
             Rotate();
@@ -58,6 +67,15 @@ public class ShipController : MonoBehaviour
         //so rotation needs to be inverted to turn in the desired direction
         rotation.z = -Input.GetAxis("Horizontal");
         transform.Rotate(rotation);
+    }
+    void CheckAltitude()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.localPosition, -transform.up, 100, mapLayer);
+        if (hit.collider == null) { return; }
+        if (altitude == hit.distance) { return; }
+        
+        altitude = hit.distance;
+        OnAltitudeChange?.Invoke(altitude);
     }
     #endregion
 }
