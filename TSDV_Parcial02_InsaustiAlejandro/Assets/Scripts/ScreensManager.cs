4012fbd60d12db414b3ad73bad86ad4c368d3300
alejandroninsausti <alejandroninsausti@gmail.com>
2021-06-30 08:24:35 -0300

    Add Functional Main Menu

---
+++ TSDV_Parcial02_InsaustiAlejandro/Assets/Scripts/ScreensManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ScreensManager : MonoBehaviour
+{
+	enum Scenes { MAIN_MENU, IN_GAME};
+
+	public void LoadMainMenu()
+    {
+		SceneManager.LoadScene("MainMenu");
+    }
+	public void LoadInGame()
+	{
+		SceneManager.LoadScene("InGame");
+	}
+	public void LoadCredits()
+	{
+		SceneManager.LoadScene("Credits");
+	}
+	public void Quit()
+    {
+		Application.Quit();
+    }
+}
