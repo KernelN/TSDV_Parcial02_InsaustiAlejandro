257877646bae7d01ae05ab0f73aee3cf28fce027
alejandroninsausti <alejandroninsausti@gmail.com>
2021-06-30 10:48:00 -0300

    Add HUD Time Update

--- TSDV_Parcial02_InsaustiAlejandro/Assets/Scripts/UIHUD.cs
+++ TSDV_Parcial02_InsaustiAlejandro/Assets/Scripts/UIHUD.cs
@@ -1,16 +1,25 @@
 ﻿using UnityEngine;
+using TMPro;
 
 public class UIHUD : MonoBehaviour
 {
 	#region Variables
-	
-	#endregion
+	[SerializeField] TextMeshProUGUI score;
+	[SerializeField] TextMeshProUGUI fuel;
+	[SerializeField] TextMeshProUGUI time;
+	[SerializeField] TextMeshProUGUI height;
+	[SerializeField] TextMeshProUGUI verticalSpeed;
+	[SerializeField] TextMeshProUGUI horizontalSpeed;
+    #endregion
 
-	#region Unity Events
-	
-	#endregion
+    #region Unity Events
+    private void Update()
+    {
+        time.text = (Time.timeSinceLevelLoad / 60).ToString("00") + ":" + (Time.timeSinceLevelLoad % 60).ToString("00");
+    }
+    #endregion
 
-	#region Methods
-	
-	#endregion
+    #region Methods
+
+    #endregion
 }
